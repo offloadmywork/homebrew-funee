0a7ae43b508b7812f97f47afee9098a048999b61
Nev Offload <nev@offloadmy.work>
2026-02-28 08:24:54 +0200

    funee 0.1.1

--- Formula/funee.rb
+++ Formula/funee.rb
@@ -1,8 +1,8 @@
 class Funee < Formula
   desc "Rust-based TypeScript runtime with macros, HTTP imports, and tree-shaking"
   homepage "https://github.com/offloadmywork/funee"
-  url "https://github.com/offloadmywork/funee/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "57e867c074fd69deaa6c9b2dadfb48c4d2f252f768d35d8add7f8ef917c69d2b"
+  url "https://github.com/offloadmywork/funee/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "c594b1788b9f16ee81fe7dafefd417ee68925d01623e6741b5630c947ccfcf8e"
   license "MIT"
 
   depends_on "rust" => :build
